3287c2efcce766c50fe909f70b871d94f0880019
dannymccolm19 <dmccolm2@gmail.com>
2022-12-04 16:56:39 -0500

    Connected database

--- src/components/pages/EventBoards.js
+++ src/components/pages/EventBoards.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react'
 import '../../App.css'
+import instance from "../../api/config";
 
-export default function EventBoard(){
-    return <h1 className='eventboard'>EVENT BOARD</h1>
+function EventBoard(){
+    const onClick = async () => {
+        const response = await instance.get(`/users`);
+        let table = document.getElementById('table');
+        let content =''
+        content += '<p>'
+        content += JSON.stringify(response.data[0].userId);
+        content += ":&nbsp;";
+        content += JSON.stringify(response.data[0].name);
+        content += '</p>'
+        table.innerHTML = content;
+    };
+    
+    return (
+    
+    <><h1 className='eventboard'>EVENT BOARD</h1>
+    <div className="button-container">
+     <button className="button"  onClick= {onClick} style={{ margin: "auto", height: 400, width: 600 }}>
+        View
+    </button>
+    </div>
+    <div id='table'></div>
+    </>
+    )
 }
+
+export default EventBoard;
